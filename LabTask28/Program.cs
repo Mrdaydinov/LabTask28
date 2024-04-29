54d1ada7f4c10e69fe4ff8e0eb0cea6da604722d
Murad <mrdaydinov@gmail.com>
2024-04-29 13:35:15 +0400

    'add prints'

--- LabTask28/Program.cs
+++ LabTask28/Program.cs
@@ -1,18 +1,37 @@
-﻿namespace LabTask28
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.WebSockets;
+
+namespace LabTask28
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("count of exams");
             int count = int.Parse(Console.ReadLine());
 
 
             List<Exam> list = new List<Exam>();
             for (int i = 0; i < count; i++)
             {
-                list.Add(
-                    new Exam(byte.Parse(Console.ReadLine()), Console.ReadLine(), DateTime.Parse(Console.ReadLine()), DateTime.Parse(Console.ReadLine()), new Student(Console.ReadLine(), Console.ReadLine(), byte.Parse(Console.ReadLine())))
-                         );
+                Console.WriteLine("Point");
+                var point = byte.Parse(Console.ReadLine());
+                Console.WriteLine("Subject");
+                var subject = Console.ReadLine();
+                Console.WriteLine("StartDate");
+                var startDate = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("EndDate");
+                var endDate = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Student firstName");
+                var firstname = Console.ReadLine();
+                Console.WriteLine("Student lastName");
+                var lastName = Console.ReadLine();
+                Console.WriteLine("Student Age");
+                var age = byte.Parse(Console.ReadLine());
+
+                list.Add(new Exam(point, subject, startDate, endDate, new Student(firstname, lastName, age)));
+
             }
 
             foreach (Exam exam in list)
